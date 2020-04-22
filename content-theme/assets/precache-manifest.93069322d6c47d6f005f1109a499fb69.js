4cc697c825b5528d4c83d5c14ada8b31c5221884
DXSh_PC <DXSh@ya.ru>
2020-04-22 15:48:01 +0300

    changes in yml, mount template directories.

---
+++ content-theme/assets/precache-manifest.93069322d6c47d6f005f1109a499fb69.js
@@ -0,0 +1,12 @@
+self.__precacheManifest = [
+  {
+    "url": "/assets/js/theme-f0d71229dea9ccfb4c53.js"
+  },
+  {
+    "url": "/assets/js/app-5f13cec35f63a06bdca8.js"
+  },
+  {
+    "revision": "f0d71229dea9ccfb4c53",
+    "url": "/assets/css/bundle-946aa822195be462fb21.css"
+  }
+];
